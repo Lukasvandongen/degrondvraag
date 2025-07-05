71e30358ea9a54001f2b1bfd3d18f811cc5b3571
BuildTools <unconfigured@null.spigotmc.org>
2025-07-06 01:44:18 +0200

    Force react build via vercel config p3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // ✅ root is je projectdir waar index.html staat
 })
